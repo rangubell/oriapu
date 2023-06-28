b82297428d5cc4eb19e0e87f447094bb5cdaede4
rangubell <rangubell@icloud.com>
2023-06-28 23:44:50 +0900

    有給付与システム
    granted_dateを超えていればtotal_dayとremaining_dayに+10するようになった

--- app/models/paid_leave.rb
+++ app/models/paid_leave.rb
@@ -6,15 +6,14 @@ class PaidLeave < ApplicationRecord
   
     paid_leaves.each do |paid_leave|
       if paid_leave.granted_date <= Date.current
-        years_since_granted = ((Date.current - paid_leave.granted_date) / 1.year).to_i
-        new_total_day = 10 + years_since_granted
-        new_remaining_day = 10 + years_since_granted
+        new_total_day = paid_leave.total_day + 10
+        new_remaining_day = paid_leave.remaining_day + 10
   
         # 有給情報を更新する
         paid_leave.update(
           total_day: new_total_day,
           remaining_day: new_remaining_day,
-          granted_date: paid_leave.granted_date + 1.year
+          granted_date: Date.current + 1.year
         )
       end
     end
