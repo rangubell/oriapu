5e705ec2f121f9d569febca8ec90142020248144
rangubell <rangubell@icloud.com>
2023-06-28 22:20:43 +0900

    有給休暇付与システムの型
    ベースが完成
    granted_dateを超えている場合10日有給が付与されるようになった

--- app/models/paid_leave.rb
+++ app/models/paid_leave.rb
@@ -3,13 +3,13 @@ class PaidLeave < ApplicationRecord
 
   def self.grant_annual_leave_if_due
     paid_leaves = PaidLeave.includes(:user).where('granted_date < ?', Date.current)
-
+  
     paid_leaves.each do |paid_leave|
-      if (paid_leave.granted_date + 1.year) <= Date.current
+      if paid_leave.granted_date <= Date.current
         years_since_granted = ((Date.current - paid_leave.granted_date) / 1.year).to_i
         new_total_day = 10 + years_since_granted
         new_remaining_day = 10 + years_since_granted
-
+  
         # 有給情報を更新する
         paid_leave.update(
           total_day: new_total_day,
