3473bcd857615924173d3c7216254519b532b0d5
rangubell <rangubell@icloud.com>
2023-06-29 01:01:33 +0900

    有給付与システムの追加
    10+勤続年数の有給付与
    一回の最大付与日数が20日
    上記の実装を確認

--- app/models/paid_leave.rb
+++ app/models/paid_leave.rb
@@ -1,13 +1,18 @@
 class PaidLeave < ApplicationRecord
   belongs_to :user
 
+  MAX_GRANT_DAYS = 20
+
   def self.grant_annual_leave_if_due
     paid_leaves = PaidLeave.includes(:user).where('granted_date < ?', Date.current)
   
     paid_leaves.each do |paid_leave|
       if paid_leave.granted_date <= Date.current
-        new_total_day = paid_leave.total_day + 10
-        new_remaining_day = paid_leave.remaining_day + 10
+        user_hired_date = paid_leave.user.hired_date
+        years_since_hired = (Date.current.year - user_hired_date.year)
+        additional_days = [MAX_GRANT_DAYS, 10 + years_since_hired].min
+        new_total_day = paid_leave.total_day + additional_days
+        new_remaining_day = paid_leave.remaining_day + additional_days
   
         # 有給情報を更新する
         paid_leave.update(
