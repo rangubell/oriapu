53781b954156167640141b0b98c202321428186e
rangubell <rangubell@icloud.com>
2023-07-10 18:35:33 +0900

    エラーメッセージ等の修正
    ####エラーメッセージが出ない場所にエラーメッセージを表示するようにし、日本語化をした。
    
    ####有給申請が同日で7/11～7/11のように申請不能だったため修正して可能にした。
    また、有給が0の場合は申請フォームが出ないように変更

--- app/models/leave_application.rb
+++ app/models/leave_application.rb
@@ -11,8 +11,8 @@ class LeaveApplication < ApplicationRecord
   validate :end_date_after_start_date
 
   def end_date_after_start_date
-    return unless end_date && start_date && end_date <= start_date
-
+    return unless end_date && start_date && end_date < start_date
+  
     errors.add(:end_date, 'must be after start date')
   end
 
