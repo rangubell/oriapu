b214bfba13450df026810ed29396fe2f0b6fc287
rangubell <rangubell@icloud.com>
2023-07-11 12:45:47 +0900

    ER図修正
    ルーティングにonryオプションを追加し必要なルーティングのみに設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "users#index"
-  resources :users, only: :show
-  resources :schedules
+  resources :users, only: [:show]
+  resources :schedules, only: [:show, :edit, :index, :new]
   resources :departments, only: [:show]
   resources :paid_leaves, only: [:new, :create, :index]
   resources :leave_applications do
     member do
       patch :update_status
     end
-  end  
+  end
 end
