8e6928ef67460815e7defd22ff16d153a1ef6ac2
rangubell <rangubell@icloud.com>
2023-07-12 15:10:28 +0900

    全体のCSS修正
    カラーリング等を一新

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "users#index"
   resources :users, only: [:show]
-  resources :schedules, only: [:show, :edit, :index, :new]
+  resources :schedules
   resources :departments, only: [:show]
   resources :paid_leaves, only: [:new, :create, :index]
   resources :leave_applications do
